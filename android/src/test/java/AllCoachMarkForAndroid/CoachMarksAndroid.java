1b67cff214ad9a631ec6d358ceb70013b63cb05e
SanthoshMahalakshmi <moai.user2@tataiq.com>
2025-06-02 18:58:40 +0530

    UPDATE: TC_039 is upto date as per the POM Architect

--- android/src/test/java/AllCoachMarkForAndroid/CoachMarksAndroid.java
+++ android/src/test/java/AllCoachMarkForAndroid/CoachMarksAndroid.java
@@ -4,6 +4,7 @@ import Actions.ElementTask;
 import DriverManagerAndroid.BaseLoginForAndroid;
 import DriverManagerAndroid.DriverManager;
 import UtilitiesForAndroid.RetryAnalyzer;
+import com.sun.jna.platform.win32.OaIdl;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
@@ -16,6 +17,7 @@ import java.util.Map;
 
 import static ElementRepositories.CoachMarkScreenElements.*;
 import static ElementRepositories.CommonElements.*;
+import static ElementRepositories.ProfileScreenElements.*;
 import static UtilitiesForAndroid.ElementActions.*;
 
 public class CoachMarksAndroid extends DriverManager {
@@ -67,6 +69,27 @@ public class CoachMarksAndroid extends DriverManager {
         elementMap.put(COACH_MARK_TEXT, COACH_MARK_TEXT_TASK);
         elementMap.put(COACH_MARK_FINISH_BUTTON, COACH_MARK_FINISH_BUTTON_TASK);
         performActions(elementMap, wait);
+    }
+
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void TC_039() throws Exception {
+        /*Global wait*/
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        /*Core Login for android.*/
+        bs.CoreLoginForAndroid(false);
+
+        Map<By, ElementTask> elementMap = new LinkedHashMap<>();
+        elementMap.put(PROFILE_BUTTON, PROFILE_BUTTON_TASK);
+        elementMap.put(COACH_MARK_TEXT, COACH_MARK_TEXT_TASK);
+        elementMap.put(COACH_MARK_NEXT_BUTTON, COACH_MARK_NEXT_BUTTON_TASK);
+        performActions(elementMap, wait);
+
+
+        Map<By, ElementTask> elementMap2 = new LinkedHashMap<>();
+        elementMap2.put(COACH_MARK_TEXT, COACH_MARK_TEXT_TASK);
+        elementMap2.put(COACH_MARK_FINISH_BUTTON, COACH_MARK_FINISH_BUTTON_TASK);
+        performActions(elementMap2, wait);
 
     }
 
--- android/src/test/java/ElementRepositories/CoachMarkScreenElements.java
+++ android/src/test/java/ElementRepositories/CoachMarkScreenElements.java
@@ -7,13 +7,18 @@ import org.openqa.selenium.By;
 
 public class CoachMarkScreenElements {
 
-    /*Report section Coach mark finish button*/
+    /*Coach mark finish button*/
     public static final By COACH_MARK_FINISH_BUTTON = AppiumBy.id("com.heartmonitor.android:id/btnNext");
     public static final ElementTask COACH_MARK_FINISH_BUTTON_TASK =
-            new ElementTask.Builder(Activity.CLICK, "Report Section: Finish").build();
+            new ElementTask.Builder(Activity.CLICK, "Coach Mark: Finish").build();
 
     /*Coach mark text*/
     public static final By COACH_MARK_TEXT = AppiumBy.id("com.heartmonitor.android:id/tvtest");
     public static final ElementTask COACH_MARK_TEXT_TASK =
             new ElementTask.Builder(Activity.VERIFY, "Coach Mark text").build();
+
+    /*Coach mark next button*/
+    public static final By COACH_MARK_NEXT_BUTTON = AppiumBy.id("com.heartmonitor.android:id/btnNext");
+    public static final ElementTask COACH_MARK_NEXT_BUTTON_TASK =
+            new ElementTask.Builder(Activity.CLICK, "Coach Mark: Next").build();
 }
