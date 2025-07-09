e28a88da40913aba1fcaf0ab40d264e3723432eb
SanthoshMahalakshmi <moai.user2@tataiq.com>
2025-07-09 12:42:19 +0530

    UPDATE:TC_023 is updated for android

--- android/src/test/java/Devices/Devices_Page_Android.java
+++ android/src/test/java/Devices/Devices_Page_Android.java
@@ -1,136 +1,41 @@
 package Devices;
 
+import Actions.ElementTask;
 import DriverManagerAndroid.BaseLoginForAndroid;
 import DriverManagerAndroid.DriverManager;
 import UtilitiesForAndroid.LogUtil;
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class Devices_Page_Android extends DriverManager
-{
+import static ElementRepositories.CommonElements.*;
+import static ElementRepositories.DeviceScreenElements.*;
+import static UtilitiesForAndroid.ElementActions.*;
+
+public class Devices_Page_Android extends DriverManager {
     BaseLoginForAndroid bs = new BaseLoginForAndroid();
-    
+
     @Test
     public void TC_023() throws Exception {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-         bs.CoreLoginForAndroid(false);  //basic login scenario
+        bs.CoreLoginForAndroid(false);  //basic login scenario
 
-        WebElement DeviceBtn, BurgerMenuBtn, ConnectDeviceBtn, ContinueBtn,
-        Permission;
-
-        //Clicking the device section
-        try {
-            DeviceBtn =  wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Devices")));
-            DeviceBtn.click();
-            LogUtil.info("The Device button is clicked to move to that page.");
-        } catch (Exception e) {
-            LogUtil.warning("Clicking device section is not happening.");
-            throw new Exception(e.getMessage());
-        }
-
-        //Verify the burger menu is clickable.
-        try {
-            BurgerMenuBtn = wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.heartmonitor.android:id/imgMenu")));
-            BurgerMenuBtn.click();
-            LogUtil.info("Burger Menu button is clicked.");
-        } catch (Exception e) {
-            LogUtil.warning("Burger menu button is not clickable.");
-        }
-
-        //Clicking on the connect device
-        try {
-            ConnectDeviceBtn = wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.
-                    androidUIAutomator("new UiSelector().text(\"Connect Device\")")));
-            ConnectDeviceBtn.click();
-            LogUtil.info("Clicking the connect Device Button.");
-        } catch (Exception e) {
-            LogUtil.warning("Clicking on connect device is not happening.");
-        }
-
-        //Clicking on continue button to find out the device
-        try {
-            ContinueBtn = wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.heartmonitor.android:id/txtContinue")));
-            ContinueBtn.click();
-            LogUtil.info("Clicking the the continue button.");
-        } catch (Exception e) {
-            LogUtil.warning("Clicking continue is not happening.");
-        }
-
-        //permission
-        try {
-            Permission = wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.android.packageinstaller:id/permission_allow_button")));
-            Permission.click();
-            LogUtil.info("Permission for bluetooth search.");
-        } catch (Exception e) {
-            LogUtil.warning("Permission for bluetooth search is not happening.");
-        }
-
-        //Clicking again on continue button to find out the device
-        try {
-            wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.heartmonitor.android:id/txtContinue"))).click();
-        } catch (Exception e) {
-            LogUtil.warning("Clicking continue again is not happening.");
-        }
-
-        //Actual device freq,
-        try {
-            wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.heartmonitor.android:id/txtDeviceName"))).click();
-        } catch (Exception e) {
-            LogUtil.warning("Actual device frequency is not showing.");
-        }
-
-        //Success message of device connection
-        try {
-            wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("com.heartmonitor.android:id/txtFailDescription")));
-            WebElement message = driver.findElement(AppiumBy.id("com.heartmonitor.android:id/txtFailDescription"));
-            LogUtil.info("Success message : " + message.getText());
-        } catch (Exception e) {
-            LogUtil.warning("Success message of connected device is not visible.");
-        }
-
-        //clicking on device info
-        try {
-            wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.heartmonitor.android:id/txtDeviceInfo"))).click();
-        } catch (Exception e) {
-            LogUtil.warning("Device info is not visible.");
-        }
-
-        //Push profile
-        try {
-            wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.heartmonitor.android:id/txtPushProfile"))).click();
-        } catch (Exception e) {
-            LogUtil.warning("Push profile is not happening.");
-        }
-
-        //Navigate home
-        try {
-            wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.
-                    androidUIAutomator("new UiSelector().description(\"Navigate up\")"))).click();
-        } catch (Exception e) {
-            LogUtil.warning("Navigating back home is not working.");
-        }
-
-        try {
-            wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("com.heartmonitor.android:id/txtEcg"))).isDisplayed();
-        } catch (Exception e) {
-            LogUtil.warning("Clicking Ecg report is not happening. ");
-        }
-
-        WebElement deviceIndication = null;
-        try {
-            deviceIndication = driver.findElement(AppiumBy.androidUIAutomator
-                    ("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(resourceId(\"com.heartmonitor.android:id/imgDeviceIndication\"));"));
-            LogUtil.info("Device indication is present : " + deviceIndication.isDisplayed());
-        } catch (Exception e) {
-            LogUtil.warning("Connected device indication is not visible.");
-        }
+        Map<By, ElementTask> elementMap = new LinkedHashMap<>();
+        elementMap.put(DEVICE_BUTTON, DEVICE_BUTTON_TASK);
+        elementMap.put(DEVICE_COACH_MARK_FINISH_BUTTON, DEVICE_COACH_MARK_FINISH_BUTTON_TASK);
+        elementMap.put(DEVICE_KEBAB_MENU, DEVICE_KEBAB_MENU_TASK);
+        elementMap.put(CONNECT_DEVICE_OPTION, CONNECT_DEVICE_OPTION_TASK);
+        elementMap.put(DELETE_DEVICE_OPTION, DELETE_DEVICE_OPTION_TASK);
+        performActions(elementMap, wait);
     }
 
     @Test
--- android/src/test/java/ElementRepositories/DeviceScreenElements.java
+++ android/src/test/java/ElementRepositories/DeviceScreenElements.java
@@ -8,7 +8,27 @@ import org.openqa.selenium.By;
 public class DeviceScreenElements {
 
     /*My Device*/
-    public static final By DEVICE_COACH_MARK_FINISH_BUTTON = AppiumBy.id("com.heartmonitor.android:id/txtMyDevice");
+    public static final By DEVICE_COACH_MARK_FINISH_BUTTON = AppiumBy.id("com.heartmonitor.android:id/btnNext");
     public static final ElementTask DEVICE_COACH_MARK_FINISH_BUTTON_TASK =
             new ElementTask.Builder(Activity.CLICK, "Device Coach mark Finish").build();
+
+    /*Device Kebab menu option*/
+    public static final By DEVICE_KEBAB_MENU = AppiumBy.id("com.heartmonitor.android:id/imgMenu");
+    public static final ElementTask DEVICE_KEBAB_MENU_TASK =
+            new ElementTask.Builder(Activity.CLICK, "Device Kebab menu").build();
+
+    /*Connect Device*/
+    public static final By CONNECT_DEVICE_OPTION = AppiumBy.androidUIAutomator("new UiSelector().text(\"Connect Device\")");
+    public static final ElementTask CONNECT_DEVICE_OPTION_TASK =
+            new ElementTask.Builder(Activity.VERIFY, "Connect Device option").build();
+
+    /*Delete Option*/
+    public static final By DELETE_DEVICE_OPTION = AppiumBy.androidUIAutomator("new UiSelector().text(\"Delete Device\")");
+    public static final ElementTask DELETE_DEVICE_OPTION_TASK =
+            new ElementTask.Builder(Activity.VERIFY, "Delete Device option").build();
+
+    /*Connected Device*/
+    public static final By CONNECTED_DEVICE_NAME = AppiumBy.androidUIAutomator("new UiSelector().text(\"Connect Device\")");
+    public static final ElementTask CONNECTED_DEVICE_NAME_TASK =
+            new ElementTask.Builder(Activity.VERIFY, "Connected device name").build();
 }
